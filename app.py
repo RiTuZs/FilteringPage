0db5b68e5fbe0cb510a403df7e61ed0e0f685002
Sukanya <sukanyasadhu17@gmail.com>
2023-10-04 17:26:15 +0530

    final commit

--- app.py
+++ app.py
@@ -1,82 +1,96 @@
+# This Python code sets up a Flask web application for uploading, filtering, and displaying data. It imports necessary modules, including Flask for web handling, Pandas for data manipulation, and SQLite3 for database management. The app initializes with a secret key and configures an upload folder. It defines routes for uploading a database file and filtering data based on user-selected checkboxes. Additionally, a route is set up to confirm successful file uploads. The code also initializes a global DataFrame, and when a file is uploaded, it reads the data into the DataFrame. Filtering is performed based on various checkbox selections, and the filtered data is displayed in an HTML template. The application starts when the script is run, and it also creates an SQLite database to store user data. Flash messages are used for user feedback during the upload and filtering processes.
+
+# Import necessary modules
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 import pandas as pd
 import sqlite3
 
+# Initialize a Flask application
 app = Flask(__name__)
-app.secret_key = "abc"
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.secret_key = "abc"  # Set a secret key for Flask session management
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Configure the upload folder for file uploads
 
 df = None  # Initialize the DataFrame globally
 
 def create_database():
+    # Create an SQLite database and a users table if it doesn't exist
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)''')
     conn.commit()
     conn.close()
 
-#this is for uploading database in html
+# This is route for uploading database in html
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     global df  # Access the global DataFrame
     if request.method == 'POST':
         if 'file' in request.files:
             file = request.files['file']
-            filename = file.filename
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            df = pd.read_excel(file_path)
-            flash("You selected a file!")
-            return render_template('index.html', data=df.to_html(index=False))
+            if file.filename == '':
+                flash("Please upload a file!")
+            else:
+                filename = file.filename
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
+                df = pd.read_excel(file_path)
+                flash("You selected a file!")
+                return render_template('index.html', data=df.to_html(index=False))
     return render_template('index.html')
 
-#this is for filtering the database in html
+#This is route for filtering the database in html
 @app.route('/filter', methods=['POST'])
 def filter_data():
     global df  # Access the global DataFrame
     if request.method == 'POST':
-        selected_state = request.form.getlist("stateCheckbox")
-        selected_district = request.form.getlist("districtCheckbox")
-        selected_clg = request.form.getlist("clgCheckbox")
-        selected_course = request.form.getlist("courseCheckbox")
-
-        if not selected_state and selected_district and selected_clg and selected_course:
-            flash("Please select at least one category.")
-            filtered_df = df
-        
+        if df is None:
+            flash("Please upload a file before searching.")
         else:
-            # Filter the DataFrame based on selected categories and types
-            if selected_state and selected_district and selected_clg and selected_course:
-                filtered_df = df[df["State"].isin(selected_state) & df["District"].isin(selected_district) & df["Types_of_Organization"].isin(selected_clg) & df["Course"].isin(selected_course)]
-            elif  selected_state and selected_district and selected_clg:
-                filtered_df = df[df["State"].isin(selected_state) & df["District"].isin(selected_district) & df["Types_of_Organization"].isin(selected_clg)]
-            elif  selected_state and selected_clg and selected_course:
-                filtered_df = df[df["State"].isin(selected_state) & df["Types_of_Organization"].isin(selected_clg) & df["Course"].isin(selected_course)]
-            elif selected_state and selected_district:
-                filtered_df = df[df["State"].isin(selected_state) & df["District"].isin(selected_district)]
-            elif selected_state and selected_clg:
-                filtered_df = df[df["State"].isin(selected_state) & df["Types_of_Organization"].isin(selected_clg)]
-            elif selected_clg and selected_course:
-                filtered_df = df[df["Types_of_Organization"].isin(selected_clg) & df["Course"].isin(selected_course)]
-            elif selected_state:
-                filtered_df = df[df["State"].isin(selected_state)]
-            elif selected_clg:
-                filtered_df = df[df["Types_of_Organization"].isin(selected_clg)]
-            else:
-                # If no checkboxes are selected, show all data
-                filtered_df = df
+            selected_state = request.form.getlist("stateCheckbox")
+            selected_district = request.form.getlist("districtCheckbox")
+            selected_clg = request.form.getlist("clgCheckbox")
+            selected_course = request.form.getlist("courseCheckbox")
 
-            data_html = filtered_df.to_html(index=False)
-            return render_template("index.html", data=data_html)
+            if not selected_state and selected_district and selected_clg and selected_course:
+                flash("Please select at least one category.")
+                filtered_df = df
+            
+            else:
+                # Filter the DataFrame based on selected categories and types
+                if selected_state and selected_district and selected_clg and selected_course:
+                    filtered_df = df[df["State"].isin(selected_state) & df["District"].isin(selected_district) & df["Types_of_Organization"].isin(selected_clg) & df["Course"].isin(selected_course)]
+                elif  selected_state and selected_district and selected_clg:
+                    filtered_df = df[df["State"].isin(selected_state) & df["District"].isin(selected_district) & df["Types_of_Organization"].isin(selected_clg)]
+                elif  selected_state and selected_clg and selected_course:
+                    filtered_df = df[df["State"].isin(selected_state) & df["Types_of_Organization"].isin(selected_clg) & df["Course"].isin(selected_course)]
+                elif selected_state and selected_district:
+                    filtered_df = df[df["State"].isin(selected_state) & df["District"].isin(selected_district)]
+                elif selected_state and selected_clg:
+                    filtered_df = df[df["State"].isin(selected_state) & df["Types_of_Organization"].isin(selected_clg)]
+                elif selected_clg and selected_course:
+                    filtered_df = df[df["Types_of_Organization"].isin(selected_clg) & df["Course"].isin(selected_course)]
+                elif selected_state:
+                    filtered_df = df[df["State"].isin(selected_state)]
+                elif selected_clg:
+                    filtered_df = df[df["Types_of_Organization"].isin(selected_clg)]
+                else:
+                    # If no checkboxes are selected, show all data
+                    filtered_df = df
+
+                data_html = filtered_df.to_html(index=False)
+                return render_template("index.html", data=data_html)
 
     flash("No data available for filtering.")
     filtered_df = df
 
+# Route for confirming successful file upload
 @app.route('/uploaded')
 def uploaded():
     return "File uploaded and data stored successfully!"
 
+# Start the Flask application
 if __name__ == '__main__':
-    create_database()
-    app.run(debug=True)
+    create_database()  # Create the database if it doesn't exist
+    app.run(debug=True)  # Start the Flask application
+
--- static/index.js
+++ static/index.js
@@ -1,98 +1,147 @@
+// This JavaScript code responds to the "DOMContentLoaded" event by executing two functions: `districtFilter()` and `collegeFilter()`. `districtFilter()` dynamically generates district checkboxes based on selected state checkboxes, while `collegeFilter()` generates course checkboxes based on selected college checkboxes, enhancing the user interface for filtering educational institutions.
+
+
+
+// This JavaScript code listens for the "DOMContentLoaded" event, which triggers when the HTML document is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
-    districtFilter();
-    collegeFilter();
+  // When the page is loaded, these two functions are called.
+  districtFilter(); // Calls the districtFilter() function.
+  collegeFilter(); // Calls the collegeFilter() function.
+});
+
+//districtFilter(): This function is responsible for dynamically populating checkboxes for districts based on the selected state checkboxes. Means when we click assam checkbox on html only "Baksa","Barpeta"....... are showing. and when we click assam,manipur checkbox it will show "Baksa","Barpeta",.........,"Bishnupur","Chandel",.......... 
+
+function districtFilter() {
+  // Data containing districts for different states.
+  const districtData = {
+      ArunachalPradesh: [
+      "Anjaw","Capital Complex Itanagar","Changlang","Dibang Valley","East Kameng","East Siang","Kamle","Kra Daadi","Kurung Kumey","Lepa Rada","Lohit","Longding","Lower Siang","Lower Subansiri","Namsai","Pakke Kessang","Papum Pare","SHI YOMI","Siang","Tawang","Tirap","Upper Siang","Upper Subansiri","West Kameng","West Siang"],
+      Assam: [
+      "Baksa","Barpeta","Bongaigaon","Cachar","Charaideo","Chirang","Darrang","Dhemaji","Dhubri","Dibrugarh","Dima Hasao","Goalpara","Golaghat","Hailakandi","Jorhat","Kamrup Metropolitan","Kamrup","Karbi Anglong","Karimganj","Kokrajhar","Lakhimpur","Majuli","Morigaon","Nagaon","Nalbari","Sivasagar","Sonitpur","South Salmara-Mankachar","Tinsukia","Udalguri","West Karbi Anglong"
+      ],
+      Manipur: ["Bishnupur","Chandel","Churachandpur","Imphal East","Imphal West","Jiribam","Kakching","Kamjong","Kangpokpi","Noney","Pherzawl","Senapati","Tamenglong","Tengnoupal","Thoubal","Ukhrul"],
+      Mizoram: ["Aizawl","Champhai","Hnahthial","Khawzawl","Kolasib","Lawngtlai","Lunglei","Mamit","Saiha","Saitual","Serchhip"],
+      Meghalaya: ["East Garo Hills","North Garo Hills","South Garo Hills","West Garo Hills","South West Garo Hills","East Khasi Hills","West Jaintia Hills","East Jaintia Hills","South West Khasi Hills","West Khasi Hills","Eastern West Khasi Hills","Ri Bhoi"],
+      Nagaland: ["Chümoukedima","Dimapur","Kiphire","Kohima","Longleng","Mokokchung","Mon","Niuland","Noklak","Peren","Phek","Shamator","Tuensang","Tseminyü","Wokha","Zünheboto"],
+      Sikkim: ["Gangtok","Mangan","Pakyong","Soreng","Namchi","Gyalshing"],
+      Tripura: ["West Tripura","North Tripura","South Tripura","Dhalai","Unakoti","Khowai","Sepahijala","Gomati"],
+  };
+
+  // Get all state checkboxes.
+  const stateCheckboxes = document.querySelectorAll('input[name="stateCheckbox"]');
+  // Get the container where district checkboxes will be added.
+  const districtCheckboxesContainer = document.getElementById("districtCheckboxes");
+  // An array to store selected districts.
+  const selectedDistricts = [];
+
+  stateCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      districtCheckboxesContainer.innerHTML = ""; // Clear previous checkboxes
+      selectedDistricts.length = 0; // Clear selected districts array
+
+      // Loop through each state checkbox
+      stateCheckboxes.forEach((stateCheckbox) => {
+        // Check if the state checkbox is checked
+        if (stateCheckbox.checked) {
+          // Get the value (state name) of the checked state checkbox
+          const stateName = stateCheckbox.value;
+          // Retrieve the list of districts for the selected state from districtData
+          const subTypes = districtData[stateName];
+
+          // Check if there are districts for the selected state and the list is not empty
+          if (subTypes && subTypes.length > 0) {
+            // Create a subheading (h4 element) to label the list of districts
+            const subHeading = document.createElement("h4");
+            subHeading.textContent = "Districts of " + stateName + ":";
+            // Append the subheading to the districtCheckboxesContainer
+            districtCheckboxesContainer.appendChild(subHeading);
+
+            // Loop through each district in the list
+            subTypes.forEach((district) => {
+              // Create a new checkbox element for the district
+              const districtCheckbox = document.createElement("input");
+              districtCheckbox.type = "checkbox";
+              districtCheckbox.name = "districtCheckbox";
+              districtCheckbox.value = district;
+
+              // Create a label element for the district checkbox
+              const label = document.createElement("label");
+              label.textContent = district;
+
+              // Append the district checkbox and label to the districtCheckboxesContainer
+              districtCheckboxesContainer.appendChild(districtCheckbox);
+              districtCheckboxesContainer.appendChild(label);
+            });
+          }
+        }
+      });
+    });
   });
-  
-//districtFilter() functions as when we click assam checkbox on html only "Baksa","Barpeta"....... are showing. and when we click assam,manipur checkbox it will show "Baksa","Barpeta",.........,"Bishnupur","Chandel",.......... 
+}
 
-  function districtFilter() {
-    const districtData = {
-        ArunachalPradesh: [
-        "Anjaw","Capital Complex Itanagar","Changlang","Dibang Valley","East Kameng","East Siang","Kamle","Kra Daadi","Kurung Kumey","Lepa Rada","Lohit","Longding","Lower Siang","Lower Subansiri","Namsai","Pakke Kessang","Papum Pare","SHI YOMI","Siang","Tawang","Tirap","Upper Siang","Upper Subansiri","West Kameng","West Siang"],
-        Assam: [
-        "Baksa","Barpeta","Bongaigaon","Cachar","Charaideo","Chirang","Darrang","Dhemaji","Dhubri","Dibrugarh","Dima Hasao","Goalpara","Golaghat","Hailakandi","Jorhat","Kamrup Metropolitan","Kamrup","Karbi Anglong","Karimganj","Kokrajhar","Lakhimpur","Majuli","Morigaon","Nagaon","Nalbari","Sivasagar","Sonitpur","South Salmara-Mankachar","Tinsukia","Udalguri","West Karbi Anglong"
-        ],
-        Manipur: ["Bishnupur","Chandel","Churachandpur","Imphal East","Imphal West","Jiribam","Kakching","Kamjong","Kangpokpi","Noney","Pherzawl","Senapati","Tamenglong","Tengnoupal","Thoubal","Ukhrul"],
-        Mizoram: ["Aizawl","Champhai","Hnahthial","Khawzawl","Kolasib","Lawngtlai","Lunglei","Mamit","Saiha","Saitual","Serchhip"],
-        Meghalaya: ["East Garo Hills","North Garo Hills","South Garo Hills","West Garo Hills","South West Garo Hills","East Khasi Hills","West Jaintia Hills","East Jaintia Hills","South West Khasi Hills","West Khasi Hills","Eastern West Khasi Hills","Ri Bhoi"],
-        Nagaland: ["Chümoukedima","Dimapur","Kiphire","Kohima","Longleng","Mokokchung","Mon","Niuland","Noklak","Peren","Phek","Shamator","Tuensang","Tseminyü","Wokha","Zünheboto"],
-        Sikkim: ["Gangtok","Mangan","Pakyong","Soreng","Namchi","Gyalshing"],
-        Tripura: ["West Tripura","North Tripura","South Tripura","Dhalai","Unakoti","Khowai","Sepahijala","Gomati"],
-    };
-  
-    const stateCheckboxes = document.querySelectorAll('input[name="stateCheckbox"]');
-    const districtCheckboxesContainer = document.getElementById("districtCheckboxes");
-    const selectedDistricts = [];
-  
-    stateCheckboxes.forEach((checkbox) => {
-      checkbox.addEventListener("change", function () {
-        districtCheckboxesContainer.innerHTML = ""; // Clear previous checkboxes
-        selectedDistricts.length = 0; // Clear selected districts array
-  
-        stateCheckboxes.forEach((stateCheckbox) => {
-          if (stateCheckbox.checked) {
-            selectedDistricts.push(...districtData[stateCheckbox.value]);
+
+//collegeFilter(): This function is responsible for dynamically populating checkboxes for courses based on the selected college checkboxes. Means when we click Diploma checkbox on html only 'Polytechnic' is showing. and when we click diploma,Engineering from  checkbox it will show 'Polytechnic',"B.Tech + B.E", "M.Tech + M.E". 
+
+function collegeFilter() {
+  // Data containing courses for different types of colleges.
+  const courseData = {
+    DiplomaCollege: ["Polytechnic"],
+    EngineeringCollege: ["BTech_BE", "MTech_ME"],
+    ComputerScienceCollege: ["BCA", "MCA"],
+    PostgraduateCollege: ["MSC"],
+    EmploymentExchangeCollege: ["EmploymentExchange"],
+    DgreeCollege: ["NotEngineeringCourses"],
+    School: ["HighSchool", "KendriyaVidyalaya"]
+  };
+
+  // Get all college checkboxes.
+  const clgCheckboxes = document.querySelectorAll('input[name="clgCheckbox"]');
+  // Get the container where course checkboxes will be added.
+  const courseCheckboxesContainer = document.getElementById("courseCheckboxes");
+  // An array to store selected courses.
+  const selectedCourses = [];
+
+  clgCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      courseCheckboxesContainer.innerHTML = ""; // Clear previous checkboxes
+      selectedCourses.length = 0; // Clear selected courses array
+
+      // Loop through each college checkbox
+      clgCheckboxes.forEach((clgCheckbox) => {
+        // Check if the college checkbox is checked
+        if (clgCheckbox.checked) {
+          // Get the value (college name) of the checked college checkbox
+          const clgName = clgCheckbox.value;
+          // Retrieve the list of courses for the selected college from courseData
+          const subCourse = courseData[clgName];
+
+          // Check if there are courses for the selected college and the list is not empty
+          if (subCourse && subCourse.length > 0) {
+            // Create a subheading (h4 element) to label the list of courses
+            const subHeading2 = document.createElement("h4");
+            subHeading2.textContent = "Different " + clgName + ":";
+            // Append the subheading to the courseCheckboxesContainer
+            courseCheckboxesContainer.appendChild(subHeading2);
+
+            // Loop through each course in the list
+            subCourse.forEach((course) => {
+              // Create a new checkbox element for the course
+              const courseCheckbox = document.createElement("input");
+              courseCheckbox.type = "checkbox";
+              courseCheckbox.name = "courseCheckbox";
+              courseCheckbox.value = course;
+
+              // Create a label element for the course checkbox
+              const label = document.createElement("label");
+              label.textContent = course;
+
+              // Append the course checkbox and label to the courseCheckboxesContainer
+              courseCheckboxesContainer.appendChild(courseCheckbox);
+              courseCheckboxesContainer.appendChild(label);
+            });
           }
-        });
-  
-        selectedDistricts.forEach((district) => {
-          const districtCheckbox = document.createElement("input");
-          districtCheckbox.type = "checkbox";
-          districtCheckbox.name = "districtCheckbox";
-          districtCheckbox.value = district;
-  
-          const label = document.createElement("label");
-          label.textContent = district;
-  
-          districtCheckboxesContainer.appendChild(districtCheckbox);
-          districtCheckboxesContainer.appendChild(label);
-        //   districtCheckboxesContainer.appendChild(document.createElement('br'));
-        });
+        }
       });
     });
-  }
-  
-//collegeFilter() functions as when we click Diploma checkbox on html only 'Polytechnic' is showing. and when we click diploma,Engineering from  checkbox it will show 'Polytechnic',"B.Tech + B.E", "M.Tech + M.E". 
-
-  function collegeFilter() {
-    const courseData = {
-      DiplomaCollege: ["Polytechnic"],
-      EngineeringCollege: ["BTech_BE", "MTech_ME"],
-      ComputerScienceCollege: ["BCA", "MCA"],
-      PostgraduateCollege: ["MSC"],
-      EmploymentExchangeCollege: ["EmploymentExchange"],
-      DgreeCollege: ["NotEngineeringCourses"],
-      School: ["HighSchool", "KendriyaVidyalaya"]
-    };
-  
-    const clgCheckboxes = document.querySelectorAll('input[name="clgCheckbox"]');
-    const courseCheckboxesContainer = document.getElementById("courseCheckboxes");
-    const selectedCourses = [];
-  
-    clgCheckboxes.forEach((checkbox) => {
-      checkbox.addEventListener("change", function () {
-        courseCheckboxesContainer.innerHTML = ""; // Clear previous checkboxes
-        selectedCourses.length = 0; // Clear selected courses array
-  
-        clgCheckboxes.forEach((clgCheckbox) => {
-          if (clgCheckbox.checked) {
-            selectedCourses.push(...courseData[clgCheckbox.value]);
-          }
-        });
-  
-        selectedCourses.forEach((course) => {
-          const courseCheckbox = document.createElement("input");
-          courseCheckbox.type = "checkbox";
-          courseCheckbox.name = "courseCheckbox";
-          courseCheckbox.value = course;
-  
-          const label = document.createElement("label");
-          label.textContent = course;
-  
-          courseCheckboxesContainer.appendChild(courseCheckbox);
-          courseCheckboxesContainer.appendChild(label);
-        //   courseCheckboxesContainer.appendChild(document.createElement('br'));
-        });
-      });
-    });
-  }
+  });
+}
